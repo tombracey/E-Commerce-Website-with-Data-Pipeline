cad347537df11f9c85deab61ecdfd3b2b00b514d
Tom Bracey <tombracey22@gmail.com>
2025-02-17 21:35:27 +0000

    added product model

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
     return render(request, 'home.html')
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
